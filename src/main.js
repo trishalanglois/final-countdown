c21463543b3c48f2497ae460b77547f10c5f9be0
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-17 10:00:25 -0700

    Get start date for GET request

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-const getDate = () => {
+const getStartDate = () => {
+  let date = new Date();
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const yyyy = String(date.getFullYear());
+  return yyyy + '-' + mm + '-01'
+}
+
+const getEndDate = () => {
   let date = new Date();
   const dd = String(date.getDate()).padStart(2, '0');
   const mm = String(date.getMonth() + 1).padStart(2, '0');
@@ -9,4 +16,7 @@ const getDate = () => {
 
 
 
-console.log(getDate())
+console.log(getStartDate(), getEndDate())
+
+
+// https://api.nasa.gov/planetary/apod?api_key=81wyuIHRfdWbPLRvS1C4zXZoQsy3GlgY2FfMUGuj&start_date=2020-02-01&end_date=2020-02-16
