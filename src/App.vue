58dd59385d6393f33dc85187ec05dc18a072ed6c
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-17 21:04:21 -0700

    Initialize listings on App render

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
-  <main id="app>
-    <ListingsContainer />
-  </main>
+    <div id="app">
+        <navbar-component></navbar-component>
+        <div class="container">
+            <listings-header-component></listings-header-component>
+            <listings-container-component 
+                v-for="listing in listings" 
+                v-bind:title="listing.title" 
+                v-bind:url="listing.url"
+                v-bind:date="listing.date" 
+                v-bind:explanation="listing.explanation"
+            ></listings-container-component>
+        </div>
+    </div>
 </template>
 
 <script>
-import ListingsContainer from './ListingsContainer.vue';
+    import ListingsContainerComponent from './components/ListingsContainerComponent'
+    import ListingsHeaderComponent from './components/ListingsHeaderComponent.vue'
+    import NavbarComponent from './components/NavbarComponent.vue'
 
-export default {
-  name: 'app',
-  components: {
-    ListingsContainer
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            NavbarComponent, 
+            ListingsHeaderComponent, 
+            ListingsContainerComponent
+        },
+        data() {
+            return {
+                listings: []
+            }
+        }
+    }
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,34 +1,10 @@
-//main header component
-Vue.component('navbar-component', {
-  template: "<header> <h1 class="main-title">Alien Zillow</h1> <h2 class='main-subtitle'>We'll help you find an outerspace you'll love.</h2> </header>'
-})
+import Vue from 'vue'
+import App from './App.vue'
 
-//subtitle component
-Vue.component('listings-header-component', {
-  template:" <h3 class='month-listings'>This Month's Listings</h3>"
-})
-
-//all listings component 
-Vue.component('listings-container-component', {
-  template: "<h4 class='listing-title'>{{ listing.title }}</h4><img class='listing-image' :src=listing.url alt='image of outerspace'><p class='listing-date'>Listing Date: {{ listing.date }}</p><p class='listing-explanation'>{{ listing.explanation }}</p>",
-  date: function() {
-    return {
-      listings: [
-        {
-          date: "2020-02-01",
-          explanation: "When leaving lunar orbit in February 1971, the crew of Apollo 14 watched this Earthrise from their command module Kittyhawk. With Earth's sunlit crescent just peeking over the lunar horizon, the cratered terrain in the foreground is along the lunar farside. Of course, while orbiting the Moon, the crew could watch Earth rise and set, but the Earth hung stationary in the sky over Fra Mauro Base, their landing site on the lunar surface. Rock samples brought back by the Apollo 14 mission included a 20 pound rock nicknamed Big Bertha, later determined to contain a likely fragment of a meteorite from planet Earth.",
-          hdurl: "https://apod.nasa.gov/apod/image/2002/AS14-71-9845v2wmktwtr4Jerry.jpg",
-          media_type: "image",
-          service_version: "v1",
-          title: "Apollo 14 Heads for Home",
-          url: "https://apod.nasa.gov/apod/image/2002/AS14-71-9845v2wmktwtr4Jerry1024.jpg"
-      }]
-    }
-  }
-})
-
-//Root Instance
 new Vue({
-  el: '#app',
-  data: {},
+  el: 'app',
+  components: {App},
+  data: {
+    listings: []
+  }
 })
