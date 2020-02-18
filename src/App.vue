7d1be54ff67957fb00610adc6377d00b0934b0cd
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-17 21:41:22 -0700

    Render hard-coded listing with Vue components

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@
         <navbar-component></navbar-component>
         <div class="container">
             <listings-header-component></listings-header-component>
-            <listings-container-component 
-                v-for="listing in listings" 
-                v-bind:title="listing.title" 
-                v-bind:url="listing.url"
-                v-bind:date="listing.date" 
-                v-bind:explanation="listing.explanation"
+            <listings-container-component :listings='this.listings'
             ></listings-container-component>
         </div>
     </div>
@@ -26,10 +21,25 @@
             ListingsHeaderComponent, 
             ListingsContainerComponent
         },
-        data() {
+        data: function() {
             return {
                 listings: []
             }
+        },
+        methods: {
+            //GET request
+        },
+        mounted() {
+            //calling GET request and setting data to response 
+            this.listings = [
+                { date: "2020-02-01",
+                  explanation: "When leaving lunar orbit in February 1971, the crew of Apollo 14 watched this Earthrise from their command module Kittyhawk. With Earth's sunlit crescent just peeking over the lunar horizon, the cratered terrain in the foreground is along the lunar farside. Of course, while orbiting the Moon, the crew could watch Earth rise and set, but the Earth hung stationary in the sky over Fra Mauro Base, their landing site on the lunar surface. Rock samples brought back by the Apollo 14 mission included a 20 pound rock nicknamed Big Bertha, later determined to contain a likely fragment of a meteorite from planet Earth.",
+                  hdurl: "https://apod.nasa.gov/apod/image/2002/AS14-71-9845v2wmktwtr4Jerry.jpg",
+                  media_type: "image",
+                  service_version: "v1",
+                  title: "Apollo 14 Heads for Home",
+                  url: "https://apod.nasa.gov/apod/image/2002/AS14-71-9845v2wmktwtr4Jerry1024.jpg"}
+            ]
         }
     }
 </script>
